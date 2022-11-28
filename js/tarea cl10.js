ddd6cc8490efa2959e9e11421e9f5bd0a46ec66c
jorge <autsanchez@hotmail.com>
2022-11-28 11:33:19 -0300

    primer commit

---
+++ js/tarea cl10.js
@@ -0,0 +1,68 @@
+let nombre ="Programacion" + "facil";
+let saludo = "Les doy la bienvenida a";
+let rd = "en Revolucion digital"
+let frasecompleta = saludo + nombre + rd;
+console.log(frasecompleta);
+
+
+
+let frase ="JavaScript" + "es un lenguaje" + "esencial para crear" + "sitios web poderosos";
+console.log(frase);
+
+
+
+let numero1= "10";
+let numero2= "20";
+let numero3= "30";
+let concatena = numero1 + numero2 + numero3;
+console.log(concatena);
+
+
+
+let n1 = "10";
+let texto1 = "es menor que"
+let n2 = "20";
+let texto2 = "y este es menor que"
+let n3 = "30";
+let concatenar = numero1 + texto1 + numero2 + texto2 + numero3;
+console.log(concatenar);
+
+
+
+let edad = parseInt (prompt ("cual es tu edad"));
+let nom = prompt ("cual es su nombre");
+let datos = nom + "tiene" + edad + "años";
+console.warn(datos);
+
+let age;
+age = parseInt(prompt("cual es tu edad?"));
+if(age >= 18){
+    alert("puedes entrar eres mayor de edad");}
+    else{ alert("No puedes entrar eres menor de edad");}
+
+
+
+
+    let sexo;
+    let ed;
+    sexo = prompt("eres hombre o mujer?"); 
+    ed = parseInt(prompt("cual es tu edad?"));
+
+    if (sexo == "hombre"){
+      if (ed >=18){alert ("puedes entrar")} else alert("no puedes entrar")};
+     
+    if (sexo == "mujer"){
+     if (ed >=20){alert ("puedes entrar")} else alert("no puedes entrar")};
+     
+
+
+     let sex;
+     let eda;
+     sex = prompt("eres hombre o mujer?"); 
+     eda = parseInt(prompt("cual es tu edad?"));
+
+    if (sex == "hombre" && eda>=18){
+    alert ("puedes entrar"); alert("no puedes entrar")};
+    
+    if (sex == "mujer" && eda>=20){
+    alert ("puedes entrar")} else alert("no puedes entrar"); 
